ec83bceea234c04efadef313243fd77a73d16907
josercc <josercc@163.com>
2023-07-20 17:28:22 +0800

    dcm list 添加-j 参数支持输出 json 字符串

--- lib/src/list_command.dart
+++ lib/src/list_command.dart
@@ -1,11 +1,17 @@
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:darty_json_safe/darty_json_safe.dart';
 import 'package:dcm/src/base_command.dart';
 import 'package:dcm/src/cli.dart';
 import 'package:dcm/src/cli_version_manager.dart';
 import 'package:mustache_template/mustache.dart';
 
 class ListCommand extends BaseCommand {
+  ListCommand() {
+    argParser.addFlag('json', abbr: 'j', help: '是否输出json格式');
+  }
+
   @override
   String get description => "list all dart commands";
 
@@ -16,6 +22,22 @@ class ListCommand extends BaseCommand {
   Future<void> run() async {
     await super.run();
     final allClis = CliVersionManager().runner.all<Cli>();
+    final json = JSON(argResults?['json']).boolValue;
+    if (json) {
+      final jsonText =
+          const JsonEncoder.withIndent(" ").convert(allClis.map((e) {
+        return {
+          'id': e.id.toString(),
+          'url': e.url,
+          'ref': e.ref,
+          'name': e.name,
+          'isLocal': e.isLocal,
+          'date': e.date?.millisecondsSinceEpoch ?? 0,
+        };
+      }).toList());
+      stdout.writeln(jsonText);
+      return;
+    }
     final cliNames = allClis.map((e) => e.name).toSet();
     final datas = cliNames.map((e) {
       final clis = allClis.where((element) => element.name == e);
