e84163ba7e5706ac64e84bacc3868c179728c1bb
josercc <josercc@163.com>
2024-05-17 16:59:19 +0800

    🟢[Add] 增加安装脚本

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+dcm_dir="$HOME/dcm"
+if [ -d "$dcm_dir" ]; then
+    rm -rf $dcm_dir
+fi
+cd $HOME
+git clone https://github.com/josercc/dcm.git
+cd dcm
+dart pub get
+dart run realm_dart install
+dart pub global activate --source path ./
+rm -rf $dcm_dir
